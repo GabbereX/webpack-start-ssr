f0bf1544dc14ccdbf4373efdfe7d7033cb7ab6b8
GabbereX <[gabberex@mail.ru]>
2022-04-21 23:34:35 +0300

    final fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,11 @@ import React from 'react';
 
 import './main.global.scss';
 import { Layout } from './shared/Layout';
-import { Header } from './shared/Header';
 import { Content } from './shared/Content';
-import { CardList } from './shared/CardsList';
 
 function AppComponent() {
   return (
     <Layout>
-      
       <Content>
       	<div>Hello World</div>
       </Content>
